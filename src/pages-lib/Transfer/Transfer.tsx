df6c238b06101bf9af0c7410bc6088bce38cce4c
Dimos Merachtsakis <dimosmera@gmail.com>
2023-03-06 11:13:13 +0700

    Add Transfer page: add Transfer page with necessary files, styles and import in _app

---
+++ src/pages-lib/Transfer/Transfer.tsx
@@ -0,0 +1,11 @@
+import styles from "./Transfer.module.css";
+
+const Transfer = () => {
+  return (
+    <div className={styles.container}>
+      <p>hey yo</p>
+    </div>
+  );
+};
+
+export default Transfer;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,20 @@
 import type { AppProps } from "next/app";
+import { Inter } from "next/font/google";
 
 import "styles/globals.css";
 
 import AppQueryClientProvider from "context/AppQueryClientProvider";
 import PhantomProvider from "context/PhantomProvider";
 
+const inter = Inter({ subsets: ["latin"] });
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AppQueryClientProvider>
       <PhantomProvider>
-        <Component {...pageProps} />
+        <main className={inter.className}>
+          <Component {...pageProps} />
+        </main>
       </PhantomProvider>
     </AppQueryClientProvider>
   );
