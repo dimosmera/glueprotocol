11025695fb95501316dbbf6437b3b40a44867e53
Dimos Merachtsakis <dimosmera@gmail.com>
2023-03-08 11:23:37 +0700

    Adjust font sizes and layout of AmountInput and AddressLink components

--- src/components/AmountInput/AmountInput.tsx
+++ src/components/AmountInput/AmountInput.tsx
@@ -10,8 +10,33 @@ interface Props {
   type: "input" | "output";
 }
 
+const MAX_AMOUNT = 100_000_000_000;
+
+// Format integer part with commas
+// Surprisingly complex to do in the input onChange since the cursor moves to the end
+// https://github.com/facebook/react/issues/955, https://stackoverflow.com/a/60131033/11688901
+const formatNumber = (value: string) => {
+  const parts = value.split(".");
+  const integer = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  const decimal = parts.length > 1 ? `.${parts[1]}` : "";
+
+  return `${integer}${decimal} USDC`;
+};
+
 const AmountInput = ({ title, type }: Props) => {
   const [focused, setFocused] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (event: React.FormEvent<HTMLInputElement>) => {
+    // Remove any non-numeric characters except decimal point
+    const value = event.currentTarget.value.replace(/[^0-9.]/g, "");
+
+    if (value && (value.split(".").length > 2 || Number(value) > MAX_AMOUNT)) {
+      return;
+    }
+
+    setInputValue(value);
+  };
 
   const handleFocus = () => {
     setFocused(true);
@@ -31,15 +56,23 @@ const AmountInput = ({ title, type }: Props) => {
           focused ? styles["blue-outline"] : "",
         ])}
       >
-        <input
-          placeholder="0"
-          type="text"
-          pattern="^\d*[.,]?\d*$"
-          inputMode="decimal"
-          onFocus={handleFocus}
-          onBlur={handleBlur}
-          className={`semi-bold-text ${styles.input}`}
-        />
+        <div className={`flexbox ${styles["numbers-container"]}`}>
+          <input
+            placeholder="0"
+            type="text"
+            inputMode="decimal"
+            value={inputValue}
+            onChange={handleInputChange}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            className={`semi-bold-text ${styles.input}`}
+          />
+          {inputValue && Number(inputValue) >= 1_000 && (
+            <p className={styles["human-readable-number"]}>
+              {formatNumber(inputValue)}
+            </p>
+          )}
+        </div>
 
         <TokenSelect type={type} />
       </div>
