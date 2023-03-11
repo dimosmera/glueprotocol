4f6dcff7b35783b3a7cd96213864f71e7af5cd6d
Dimos Merachtsakis <dimosmera@gmail.com>
2023-03-11 16:05:44 +0700

    Add favicon and adjust font size in design tokens

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,3 +1,6 @@
+import NextImage from "next/image";
+import NextLink from "next/link";
+
 import displayAddress from "utils/displayAddress";
 import useGetPhantomContext from "context/PhantomProvider/useGetPhantomContext";
 
@@ -29,18 +32,31 @@ const AppHeader = () => {
 
   return (
     <div className={`flexbox ${styles.container}`}>
-      <a
-        className={styles["help-text"]}
-        href="https://twitter.com/dimos851"
-        target="_blank"
-        rel="noopener"
-      >
-        Help
-      </a>
+      <NextLink href="/" className={`flexbox ${styles["title-container"]}`}>
+        <NextImage
+          src="/glue-icon.png"
+          alt="Glue protocol logo"
+          width={46}
+          height={46}
+        />
 
-      <button className={styles["connect-button"]} onClick={handleConnect}>
-        {!publicKey ? "Connect Wallet" : displayAddress(publicKey.toString())}
-      </button>
+        <h1 className={`bold-text ${styles["app-title"]}`}>Glue</h1>
+      </NextLink>
+
+      <div className="flexbox">
+        <a
+          className={styles["help-text"]}
+          href="https://twitter.com/dimos851"
+          target="_blank"
+          rel="noopener"
+        >
+          Help
+        </a>
+
+        <button className={styles["connect-button"]} onClick={handleConnect}>
+          {!publicKey ? "Connect Wallet" : displayAddress(publicKey.toString())}
+        </button>
+      </div>
     </div>
   );
 };
