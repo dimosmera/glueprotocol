32b0d793aca95590ff0f8ead35c47894c1553546
Dimos Merachtsakis <dimosmera@gmail.com>
2023-03-12 17:58:37 +0700

    Add localStorage service functions to index.ts

---
+++ src/services/localStorage/index.ts
@@ -0,0 +1,30 @@
+export const setItemToLocalStorage = (key: string, value: any) => {
+  try {
+    if (!localStorage) return;
+
+    localStorage.setItem(key, value);
+  } catch (e) {
+    return;
+  }
+};
+
+export const getItemFromLocalStorage = (key: string) => {
+  try {
+    if (!localStorage) return;
+
+    return localStorage.getItem(key);
+  } catch (e) {
+    return;
+  }
+};
+
+export const removeItemFromLocalStorage = (key: string) => {
+  try {
+    if (!localStorage) return;
+    if (!localStorage.getItem(key)) return;
+
+    localStorage.removeItem(key);
+  } catch (e) {
+    return;
+  }
+};
