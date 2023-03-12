350dc5d089361fca8e4ef9517c492fcf40ce7c8f
Dimos Merachtsakis <dimosmera@gmail.com>
2023-03-12 13:30:39 +0700

    Add dealWithMWAErrors utility to handle MWA errors

---
+++ src/utils/dealWithMWAErrors.ts
@@ -0,0 +1,31 @@
+import {
+  SolanaMobileWalletAdapterProtocolErrorCode,
+} from "@solana-mobile/mobile-wallet-adapter-protocol";
+
+import { fireErrorAlert } from "components/SweetAlerts";
+
+/**
+ * MWA has more error codes. If you need them, add them to the switch
+ */
+const dealWithMWAErrors = (error: { code: number } | undefined) => {
+  if (!error) {
+    fireErrorAlert();
+    return;
+  }
+
+  switch (error.code) {
+    case SolanaMobileWalletAdapterProtocolErrorCode.ERROR_AUTHORIZATION_FAILED:
+      fireErrorAlert("Connection rejected");
+      break;
+
+    case SolanaMobileWalletAdapterProtocolErrorCode.ERROR_NOT_SIGNED:
+      fireErrorAlert("Sign request declined");
+      break;
+
+    default:
+      fireErrorAlert();
+      break;
+  }
+}
+
+export default dealWithMWAErrors;
