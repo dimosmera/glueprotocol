bac624d83b1be1e12ab8dfac5c58fcac5dc33c82
Dimos Merachtsakis <dimosmera@gmail.com>
2023-03-13 01:14:19 +0700

    Update Droplinks API call to use 'Glue links' campaign and 'SPL' dropLinkType

--- src/services/api/useCreateDroplink/useCreateDroplink.ts
+++ src/services/api/useCreateDroplink/useCreateDroplink.ts
@@ -6,7 +6,7 @@ const useCreateDroplink = () => {
   return useMutation(async () =>
     http().post(
       "https://droplinks.io/api/v1/drop-links/create/",
-      { campaign: "glow-test", dropLinkType: "SPL" },
+      { campaign: "Glue links", dropLinkType: "SPL" },
       { headers: { "X-API-KEY": process.env.NEXT_PUBLIC_DROPLINKS_API_KEY } }
     )
   );
