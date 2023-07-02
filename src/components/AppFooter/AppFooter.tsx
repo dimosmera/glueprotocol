7742f6895cc0e81af4a541797acb9404b95f21a5
Dimos Merachtsakis <dimosmera@gmail.com>
2023-07-02 11:26:09 +0300

    Adds Fork on GitHub link

--- src/components/AppFooter/AppFooter.tsx
+++ src/components/AppFooter/AppFooter.tsx
@@ -1,28 +1,44 @@
 import { FiHeart } from "react-icons/fi";
 
+import classList from "utils/classList";
+
 import styles from "./AppFooter.module.css";
 
 const AppFooter = () => {
   return (
     <div className={`flexbox ${styles.container}`}>
-      <p className={styles["love-text"]}>Made with</p>
+      <div className="flexbox">
+        <p className={styles["love-text"]}>Made with</p>
 
-      <FiHeart
-        size={14}
-        color="#1D243C"
-        fill="red"
-        style={{ marginLeft: "4px", marginTop: "3px", marginRight: "4px" }}
-      />
+        <FiHeart
+          size={14}
+          color="#1D243C"
+          fill="red"
+          style={{ marginLeft: "4px", marginTop: "3px", marginRight: "4px" }}
+        />
 
-      <p className={styles["love-text"]}>by</p>
+        <p className={styles["love-text"]}>by</p>
+
+        <a
+          className={styles["link-text"]}
+          href="https://twitter.com/dimos851"
+          target="_blank"
+          rel="noopener"
+        >
+          dimos851
+        </a>
+      </div>
+
+      <p className={styles["divider-dot"]}>•</p>
 
       <a
-        className={styles["link-text"]}
-        href="https://twitter.com/dimos851"
+        href="https://github.com/dimosmera/glueprotocol/"
         target="_blank"
         rel="noopener"
       >
-        dimos851
+        <p className={classList([styles["link-text"], styles["no-margin"]])}>
+          Fork on GitHub
+        </p>
       </a>
     </div>
   );
